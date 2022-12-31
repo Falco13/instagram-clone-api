6ea98caf982a48192905bcf0b28ce4fb26f5f23c
falco13 <keanu0608@gmail.com>
2022-12-31 15:25:54 +0200

    get all posts

--- routers/post.py
+++ routers/post.py
@@ -3,7 +3,8 @@ from fastapi.exceptions import HTTPException
 from routers.schemas import PostBase, PostDisplay
 from sqlalchemy.orm.session import Session
 from db.database import get_db
-from db.db_post import create_post_db
+from db.db_post import create_post_db, get_all
+from typing import List
 
 router = APIRouter(prefix='/post',
                    tags=['post'],
@@ -18,3 +19,8 @@ def create_post(request: PostBase, db: Session = Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
                             detail="Parameter image_url_type can only take values 'absolute' or 'relative'.")
     return create_post_db(db, request)
+
+
+@router.get('/all', response_model=List[PostDisplay])
+def all_posts(db: Session = Depends(get_db)):
+    return get_all(db)
